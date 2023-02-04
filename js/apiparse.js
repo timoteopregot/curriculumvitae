7f0ecf275b4208c3fb9a9768d5e766988f1a053c
timoteo <timoteo.pregot@gmail.com>
2023-02-04 13:26:30 -0300

    subida actualizada, carga todos los datos del usuario, falta terminar el cv

--- js/apiparse.js
+++ js/apiparse.js
@@ -2,6 +2,11 @@ var nombre= document.getElementById('fullname');
 var mail=document.getElementById('mail');
 var phone=document.getElementById('phone');
 var thumb = document.getElementById('photo');
+var edad = document.getElementById('age');
+var nac = document.getElementById('birth');
+var dir = document.getElementById('address');
+
+
 
 function fetchUser(){
   
@@ -12,11 +17,21 @@ function fetchUser(){
             console.log(res.results[0].name)
 
             let thumbnailimg=res.results[0].picture.large
+            let fnac= res.results[0].dob.date
+            fecha= fnac.slice(0,10)
+
 
             nombre.innerHTML= res.results[0].name.first +" "+ res.results[0].name.last
             mail.innerHTML= res.results[0].email
             phone.innerHTML= res.results[0].phone
             thumb.src= thumbnailimg
+            edad.innerHTML= res.results[0].dob.age
+            nac.innerHTML =  fecha
+            dir.innerHTML =  res.results[0].location.street.name + ", " + res.results[0].location.street.number + " "+ res.results[0].location.state +","+res.results[0].location.country
+          
+          
+          
+          
       
       
       
